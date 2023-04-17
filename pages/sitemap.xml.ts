825f148da69180fb73ed183bb3849cf914802caa
BeLeap <beleap@beleap.dev>
2023-04-18 01:56:23 +0900

    add sitemap

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,44 @@
+import { filenameToSlug, getPostFilenames } from "@/utils/posts"
+import { GetServerSideProps } from "next"
+
+const host = "https://beleap.dev"
+
+function generateSiteMap(slugs: string[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>${host}</loc>
+     </url>
+     ${slugs
+      .map((slug) => {
+        return `
+       <url>
+           <loc>${`${host}/${slug}`}</loc>
+       </url>
+     `;
+      })
+      .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const filenames = await getPostFilenames()
+  const sitemap = generateSiteMap(filenames.map((filename) => filenameToSlug(filename)));
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
