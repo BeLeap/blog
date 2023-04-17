c69983adb2a0f99af23f6e30ad3f1f9aa999a801
BeLeap <beleap@beleap.dev>
2023-04-17 21:50:24 +0900

    update layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,10 +2,10 @@ export default function Layout(props: any) {
   console.log(props)
 
   return (
-    <>
-      <header>Navigation</header>
-      <main>{props.children}</main>
-      <footer>Footer</footer>
-    </>
+    <div className="flex flex-col min-h-screen container mx-auto">
+      <header className="">Navigation</header>
+      <main className="flex-1 flex-col flex flex-wrap items-top justify-content">{props.children}</main>
+      <footer className="">Footer</footer>
+    </div>
   )
 }
