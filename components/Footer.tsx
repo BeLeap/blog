026a9ee81ffeee3aa3623388ddd4747e60ea9805
BeLeap <beleap@beleap.dev>
2023-04-21 18:48:18 +0900

    add link to rss

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,7 @@
+import palette from "@catppuccin/palette"
+import Link from "next/link"
+import { FaRss } from "react-icons/fa"
+
 export default function Footer() {
   return (
     <footer
@@ -5,10 +9,26 @@ export default function Footer() {
         display: "flex",
         width: "100%",
         justifyContent: "center",
+        alignItems: "center",
         gap: "0.5rem",
       }}
     >
       {`${(new Date()).getFullYear()} © BeLeap`}
+      {" / "}
+      <Link
+        href="/atom.xml"
+        style={{
+          margin: 0,
+          display: "flex",
+          alignItems: "center",
+          gap: "0.2rem",
+          textDecoration: "none",
+          borderBottom: `1px solid ${palette.variants.mocha.blue.hex}`,
+        }}
+      >
+        <FaRss fill={`${palette.variants.mocha.blue.hex}`} />
+        RSS
+      </Link>
     </footer>
   )
 }
