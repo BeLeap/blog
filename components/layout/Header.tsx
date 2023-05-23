532ab2a7c665ccc11a7b401aea77b5c75beb35eb
BeLeap <beleap@beleap.dev>
2023-05-23 22:21:43 +0900

    write bio

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,19 @@
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="flex gap-4">
+      <img
+        src="/profile.png"
+        alt="profile picture"
+        className="rounded-lg w-36 h-36"
+      />
+      <div className="flex flex-col gap-4 justify-around">
+        <div className="text-4xl font-bold text-white">BeLeap</div>
+        <p className="text-xl whitespace-break-spaces">
+          DevOps engineer by day,{"\n"}
+          Rust enthusiast by night.{"\n"}
+          Sticks to Neovim.
+        </p>
+      </div>
     </div>
   );
 };
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Head } from "$fresh/runtime.ts";
 import { ComponentChildren } from "preact";
 import Header from "./Header.tsx";
+import * as mocha from "../../consts/mocha.ts";
 
 type Props = {
   children: ComponentChildren;
@@ -8,13 +9,15 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <>
+    <div
+      className={`w-screen h-screen flex flex-col items-center bg-[${mocha.Surface0}] text-[${mocha.Overlay2}] gap-4 p-4`}
+    >
       <Head>
         <title>Beleap Blog</title>
       </Head>
       <Header />
       {children}
-    </>
+    </div>
   );
 };
 export default Layout;
