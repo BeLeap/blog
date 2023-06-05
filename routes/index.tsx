b821b467840b02e89758acbf87da8a5218da3558
BeLeap <beleap@beleap.dev>
2023-06-05 15:37:46 +0900

    load frontmatter

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,7 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import ArticleCard from "../components/ArticleCard.tsx";
 import Layout from "../components/layout/Layout.tsx";
 import type * as Article from "../types/articles.ts";
+import { parse } from "https://deno.land/x/frontmatter@v0.1.5/mod.ts";
 
 interface Props {
   articleMetas: Article.Metadata[];
@@ -9,20 +10,41 @@ interface Props {
 
 export const handler: Handlers<Props> = {
   async GET(_req, ctx) {
-    const articleMetas: Article.Metadata[] = [
-      {
-        path: "lorem",
-        title: "Lorem",
-        summary: "lorem",
-        time: new Date(),
-      },
-      {
-        path: "ipsum",
-        title: "Ipsum",
-        summary: "ipsum",
-        time: new Date(),
-      },
-    ];
+    const articleMetas: Article.Metadata[] = [];
+    for await (const dirEntry of Deno.readDir("posts")) {
+      // NOTE: Only support top-level articles.
+      if (dirEntry.isFile) {
+        const path = `posts/${dirEntry.name}`;
+        const content = await Deno.readTextFile(path);
+        const parsedResult = parse(content);
+        const metadata = parsedResult.data as {
+          title: string;
+          published_at: string;
+          updated_at: string;
+          summary: string;
+        };
+        const articleMetadata: Article.Metadata = {
+          path,
+          title: metadata.title,
+          summary: metadata.summary,
+          time: new Date(metadata.updated_at),
+        };
+
+        articleMetas.push(articleMetadata);
+      }
+    }
+
+    articleMetas.sort((a, b) => {
+      if (a.time > b.time) {
+        return -1;
+      }
+
+      if (a.time < b.time) {
+        return 1;
+      }
+
+      return 0;
+    });
 
     return ctx.render({ articleMetas });
   },
