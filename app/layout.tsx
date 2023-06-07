72f0834be960c0ec23e2e08b7ec366f02ceb590d
BeLeap <beleap@beleap.dev>
2023-06-08 01:39:28 +0900

    add ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
       <body className={styles.body}>
         <Header />
         {children}
