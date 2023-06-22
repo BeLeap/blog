0323054ee31fd06d60a386c9708912f19a48ec82
BeLeap <beleap@beleap.dev>
2023-06-22 10:01:04 +0900

    better semantic tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,38 +3,36 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <a href="https://github.com/BeLeap">
-            Github
-          </a>
-          <a href="https://twitter.com/beleap_">
-            Twitter
-          </a>
-          <a href="mailto:beleap@beleap.dev">
-            Mail
-          </a>
-          <Image
-            src="/profile.png"
-            alt="profile picture"
-            width={100}
-            height={100}
-            className={styles.profile}
-          />
-        </div>
-        <div className={styles.right}>
-          <div className={styles.name}>
-            BeLeap
-          </div>
-          <p className={styles.description}>
-            DevOps engineer by day,{"\n"}
-            Rust enthusiast by night.{"\n"}
-            Sticks to Neovim.
-          </p>
-        </div>
+    <header className={styles.container}>
+      <div className={styles.left}>
+        <a href="https://github.com/BeLeap">
+          Github
+        </a>
+        <a href="https://twitter.com/beleap_">
+          Twitter
+        </a>
+        <a href="mailto:beleap@beleap.dev">
+          Mail
+        </a>
+        <Image
+          src="/profile.png"
+          alt="profile picture"
+          width={100}
+          height={100}
+          className={styles.profile}
+        />
       </div>
-    </>
+      <div className={styles.right}>
+        <div className={styles.name}>
+          BeLeap
+        </div>
+        <p className={styles.description}>
+          DevOps engineer by day,{"\n"}
+          Rust enthusiast by night.{"\n"}
+          Sticks to Neovim.
+        </p>
+      </div>
+    </header>
   );
 };
 export default Header;
