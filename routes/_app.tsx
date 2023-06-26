c9c1858bb02eddfc12f289050309ee8f5d93850d
BeLeap <beleap@beleap.dev>
2023-06-26 14:15:57 +0900

    remove default contents

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,9 +1,6 @@
-import Header from "../components/Header.tsx";
-
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
       {children}
     </>
   );
