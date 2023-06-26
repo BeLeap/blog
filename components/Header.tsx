3894a794009f45db261d7cb3801c160ad9278532
BeLeap <beleap@beleap.dev>
2023-06-26 14:43:12 +0900

    add header

---
+++ components/Header.tsx
@@ -0,0 +1,81 @@
+import { css } from "@emotion/css";
+
+const Header = () => {
+  return (
+    <header
+      className={css`
+        display: flex;
+        margin-bottom: 2.5rem;
+        padding: 0.5rem;
+        align-items: center;
+      `}
+    >
+      <address
+        className={css`
+          display: grid;
+          grid-template-rows: repeat(4, minmax(0, 1fr));
+          grid-auto-flow: column;
+          max-height: 12rem;
+          padding: 1rem;
+          gap: 1rem;
+        `}
+      >
+        <a href="https://github.com/BeLeap">
+          Github
+        </a>
+        <a href="https://twitter.com/beleap_">
+          Twitter
+        </a>
+        <a href="mailto:beleap@beleap.dev">
+          Mail
+        </a>
+        <img
+          src="/assets/profile.png"
+          alt="profile picture"
+          width={100}
+          height={100}
+          className={css`
+            width: 2rem;
+            height: 2rem;
+            order: -9999;
+          `}
+        />
+      </address>
+      <section
+        className={css`
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          gap: 1rem;
+          padding: 1rem;
+
+          border-left: solid;
+          border-left-width: 1px;
+          border-left-color: gray;
+        `}
+      >
+        <h1
+          className={css`
+            font-size: 2.25rem;
+            line-height: 2.5rem;
+            font-weight: 700;
+          `}
+        >
+          BeLeap
+        </h1>
+        <p
+          className={css`
+            font-size: 1.25rem;
+            line-height: 1.75rem;
+            white-space: break-spaces;
+          `}
+        >
+          DevOps engineer by day,{"\n"}
+          Rust enthusiast by night.{"\n"}
+          Sticks to Neovim.
+        </p>
+      </section>
+    </header>
+  );
+};
+export default Header;
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,6 +1,39 @@
+import Header from "@/components/Header.tsx";
+import { injectGlobal } from "@emotion/css";
+
+injectGlobal`
+  @import url('https://unpkg.com/@highlightjs/cdn-assets@11.8.0/styles/default.min.css');
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap');
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  * {
+    font-family: 'IBM Plex Mono', 'Nanum Gothic Coding', monospace;
+  }
+
+  pre {
+    border: solid 1px;
+    border-radius: 1rem;
+    padding: 1rem;
+    overflow: scroll;
+  }
+
+  a {
+    color: gray;
+  }
+
+  a:hover {
+    color: #862633;
+  }
+`;
+
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Header />
       {children}
     </>
   );
