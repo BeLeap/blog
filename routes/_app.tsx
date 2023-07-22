9f475a5bc119fedaadf8d7f503c37fb98dce0bd6
BeLeap <beleap@beleap.dev>
2023-07-22 23:07:17 +0900

    update css

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,35 +7,36 @@ export default function App({ children }: { children: React.ReactNode }) {
       <Head>
         <link rel="icon" href="/assets/favicon.ico" />
         <style>{`
-          @import url('https://unpkg.com/@highlightjs/cdn-assets@11.8.0/styles/default.min.css');
-          @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap');
-          @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap');
-
-          ::-webkit-scrollbar {
-            display: none;
-          }
-
-          * {
-            font-family: 'IBM Plex Mono', 'Nanum Gothic Coding', monospace;
-          }
-
-          pre {
-            border: solid 1px;
-            border-radius: 1rem;
-            padding: 1rem;
-            overflow: scroll;
-          }
-
-          a {
-            color: gray;
-          }
-
-          a:hover {
-            color: #862633;
-          }  
         `}</style>
       </Head>
       <style>{`
+        @import url('https://unpkg.com/@highlightjs/cdn-assets@11.8.0/styles/default.min.css');
+        @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap');
+
+        ::-webkit-scrollbar {
+          display: none;
+        }
+
+        * {
+          font-family: 'IBM Plex Mono', 'Nanum Gothic Coding', monospace;
+        }
+
+        pre {
+          border: solid 1px;
+          border-radius: 1rem;
+          padding: 1rem;
+          overflow: scroll;
+        }
+
+        a {
+          color: gray;
+        }
+
+        a:hover {
+          color: #862633;
+        }  
+
         ._app__container {
           display: flex;
           flex-direction: column;
