26af88ef1bd38efa6a8e74b43754ba3e7e098350
BeLeap <beleap@beleap.dev>
2023-10-22 23:52:28 +0900

    setup header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,29 @@
 const Header = () => {
   return (
-    <header>
-      This is header
+    <header class="p-4 flex items-center justify-between">
+      <div class="flex gap-2 items-center">
+        <img
+          src="/profile.png"
+          class="w-16"
+        />
+        <h1 class="font-black text-4xl">
+          BeLeap Blog
+        </h1>
+      </div>
+      <nav class="flex gap-4">
+        {[
+          ["Home", "/"],
+          ["About", "/about"],
+        ].map((it) => (
+          <a
+            href={it[1]}
+            class="text-gray-400 font-bold text-xl hover:text-black"
+          >
+            {it[0]}
+          </a>
+        ))}
+      </nav>
     </header>
-  )
-}
+  );
+};
 export default Header;
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header.tsx";
 import { injectGlobal } from "@twind/core";
 
 injectGlobal`
-`
+`;
 
 export default function App({ Component }: AppProps) {
   return (
@@ -11,7 +11,10 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="https://cdn.jsdelivr.net/gh/naen-nae/fonts@purge-cache-for-subsets/build/css/IBMPlexSansKR.css" rel="stylesheet" />
+        <link
+          href="https://cdn.jsdelivr.net/gh/naen-nae/fonts@purge-cache-for-subsets/build/css/IBMPlexSansKR.css"
+          rel="stylesheet"
+        />
         <link rel="stylesheet" href="/global.css" />
         <title>BeLeap Blog</title>
       </head>
