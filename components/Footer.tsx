375b1de433eea8f9de7950b4c5fe7d93d6d62832
BeLeap <beleap@beleap.dev>
2023-11-02 01:17:07 +0900

    better footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,21 +4,25 @@ import IconCopyright from "@icon/copyright.tsx";
 
 const Footer = () => {
   return (
-    <div class="fixed bottom-0 left-0 w-[100%] bg-white p-4 flex items-center justify-between">
-      <div class="flex gap-2">
-        {[
-          { url: "https://github.com/BeLeap", label: <IconBrandGitHub /> },
-          {
-            url: "https://social.silicon.moe/@beleap",
-            label: <IconBrandMastodon />,
-          },
-        ].map((it) => (
-          <a class="text-gray-400 hover:text-black" href={it.url}>{it.label}</a>
-        ))}
-      </div>
-      <div class="flex gap-1 text-lg font-medium items-center">
-        <IconCopyright />
-        BeLeap
+    <div class="fixed bottom-0 left-0 w-full flex items-center justify-center">
+      <div class="max-w-4xl w-full flex items-center justify-between p-4 bg-white">
+        <div class="flex gap-2">
+          {[
+            { url: "https://github.com/BeLeap", label: <IconBrandGitHub /> },
+            {
+              url: "https://social.silicon.moe/@beleap",
+              label: <IconBrandMastodon />,
+            },
+          ].map((it) => (
+            <a class="text-gray-400 hover:text-black" href={it.url}>
+              {it.label}
+            </a>
+          ))}
+        </div>
+        <div class="flex gap-1 text-lg font-medium items-center">
+          <IconCopyright />
+          BeLeap
+        </div>
       </div>
     </div>
   );
