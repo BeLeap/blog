942ccc908bad6157045872087dd22ab370083613
BeLeap <beleap@beleap.dev>
2023-11-02 01:23:11 +0900

    fix header color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import IconCopyright from "@icon/copyright.tsx";
 
 const Footer = () => {
   return (
-    <div class="fixed bottom-0 left-0 w-full flex items-center justify-center">
-      <div class="max-w-4xl w-full flex items-center justify-between p-4 bg-white">
+    <div class="fixed bottom-0 left-0 w-full flex items-center justify-center bg-white">
+      <div class="max-w-4xl w-full flex items-center justify-between p-4">
         <div class="flex gap-2">
           {[
             { url: "https://github.com/BeLeap", label: <IconBrandGitHub /> },
