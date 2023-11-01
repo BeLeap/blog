6545b027da73e91db0dc7c4954570e08727f9cac
BeLeap <beleap@beleap.dev>
2023-11-01 23:44:05 +0900

    feat: markdown rendering

--- routes/post/[slug].tsx
+++ routes/post/[slug].tsx
@@ -1,9 +1,41 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { CSS, KATEX_CSS, render } from "https://deno.land/x/gfm@0.2.5/mod.ts";
+import * as posts from "../../lib/posts.ts";
+import { Head } from "$fresh/runtime.ts";
+import { Just } from "../../lib/types.ts";
 
-const PostPage = (props: PageProps) => {
+interface PostPageData {
+  content: string;
+}
+export const handler: Handlers<PostPageData> = {
+  async GET(req, ctx) {
+    const postContent = await posts.getContent(`${ctx.params.slug}.md`);
+    const maybePostParsed = posts.parseFrontmatter(postContent);
+
+    if (Just.isJust(maybePostParsed)) {
+      const postParsed = maybePostParsed.unwrap();
+      const postContent = postParsed[1];
+      const content = render(postContent, {
+        baseUrl: req.url,
+        allowMath: true,
+      });
+      return ctx.render({ content });
+    }
+    return ctx.render(undefined);
+  },
+};
+
+const PostPage = (props: PageProps<PostPageData>) => {
   return (
     <>
-      {props.params.slug}
+      <Head>
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
+        <style dangerouslySetInnerHTML={{ __html: KATEX_CSS }} />
+      </Head>
+      <div
+        class="markdown-body"
+        dangerouslySetInnerHTML={{ __html: props.data.content }}
+      />
     </>
   );
 };
