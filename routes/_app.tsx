3591f590603a5a7d0e3d28e0c1e77d6b3199c2cf
BeLeap <beleap@beleap.dev>
2023-11-02 01:22:42 +0900

    better right bar handling

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -21,9 +21,13 @@ const App = ({ Component }: AppProps) => {
           <div class="max-w-7xl w-full col-start-1 col-span-4 row-start-2 row-span-7 md:col-span-3 xl:col-start-2 xl:col-span-2">
             <Component />
           </div>
-          <div class="justify-self-start col-span-2 md:col-start-4 md:row-start-2 md:row-span-2 flex flex-col gap-4">
-            <Categories />
-            <RecentPosts />
+          <div class="justify-self-start w-full col-span-4 md:col-start-4 md:row-start-2 md:row-span-2 grid grid-cols-2 grid-rows-2 gap-4 px-4">
+            <div class="col-start-1 col-span-1 row-start-1 row-span-2 md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-1">
+              <Categories />
+            </div>
+            <div class="col-start-2 col-span-1 row-start-1 row-span-2 md:col-start-1 md:col-span-2 md:row-start-2 md:row-span-1">
+              <RecentPosts />
+            </div>
           </div>
           <Footer />
         </div>
