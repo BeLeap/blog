666cd98de74b73ceb7cfd995df149941ce4ca1dd
BeLeap <beleap@beleap.dev>
2023-11-05 15:03:06 +0900

    remove recent posts

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from "$fresh/server.ts";
 import Footer from "../components/Footer.tsx";
 import Header from "../components/Header.tsx";
-import RecentPosts from "../components/RecentPosts.tsx";
 
 const App = ({ Component }: AppProps) => {
   return (
@@ -19,9 +18,6 @@ const App = ({ Component }: AppProps) => {
           </div>
           <div class="max-w-7xl w-full col-start-1 col-span-4 row-start-2 row-span-7 md:col-span-3 xl:col-start-2 xl:col-span-2">
             <Component />
-          </div>
-          <div class="justify-self-start w-full col-span-4 md:col-start-4 md:row-start-2 md:row-span-2 p-4">
-            <RecentPosts />
           </div>
           <Footer />
         </div>
